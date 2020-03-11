b003818b4551a92169b4f622873bf126d9f81433
Alonso Herrera <alonsohf055@gmail.com>
2020-03-10 22:26:30 -0600

    Horarios class has been created

---
+++ src/Horario.java
@@ -0,0 +1,62 @@
+
+public class Horario {
+int cod_Horario;
+int num_bus;
+String desc_Horario;
+String tiempo_Salida;
+String tiempo_llegada;
+Boolean estado;
+ 
+
+
+public Horario(int cod_Horario, int num_bus, String desc_Horario, String tiempo_Salida, String tiempo_llegada,
+		Boolean estado) {
+	this.cod_Horario = cod_Horario;
+	this.num_bus = num_bus;
+	this.desc_Horario = desc_Horario;
+	this.tiempo_Salida = tiempo_Salida;
+	this.tiempo_llegada = tiempo_llegada;
+	this.estado = estado;
+}
+public Horario() {
+	
+}
+
+public int getCod_Horario() {
+	return cod_Horario;
+}
+public void setCod_Horario(int cod_Horario) {
+	this.cod_Horario = cod_Horario;
+}
+public int getNum_bus() {
+	return num_bus;
+}
+public void setNum_bus(int num_bus) {
+	this.num_bus = num_bus;
+}
+public String getDesc_Horario() {
+	return desc_Horario;
+}
+public void setDesc_Horario(String desc_Horario) {
+	this.desc_Horario = desc_Horario;
+}
+public String getTiempo_Salida() {
+	return tiempo_Salida;
+}
+public void setTiempo_Salida(String tiempo_Salida) {
+	this.tiempo_Salida = tiempo_Salida;
+}
+public String getTiempo_llegada() {
+	return tiempo_llegada;
+}
+public void setTiempo_llegada(String tiempo_llegada) {
+	this.tiempo_llegada = tiempo_llegada;
+}
+public Boolean getEstado() {
+	return estado;
+}
+public void setEstado(Boolean estado) {
+	this.estado = estado;
+}
+
+}
