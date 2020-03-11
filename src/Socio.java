a986918b57e71c30dcef22a486440dff0dfc4f8a
Alonso Herrera <alonsohf055@gmail.com>
2020-03-10 23:43:33 -0600

    Socio class has been created

---
+++ src/Socio.java
@@ -0,0 +1,86 @@
+
+public class Socio {
+int cod_Socio;
+String nombre;
+String apellidos;
+String linea;
+int num_Bus;
+int cap_Bus;
+String ayudante;
+String chofer;
+Boolean estado;
+
+public Socio() {
+}
+public Socio(int cod_Socio, String nombre, String apellidos, String linea, int num_Bus, int cap_Bus, String ayudante,
+		String chofer, Boolean estado) {
+	this.cod_Socio = cod_Socio;
+	this.nombre = nombre;
+	this.apellidos = apellidos;
+	this.linea = linea;
+	this.num_Bus = num_Bus;
+	this.cap_Bus = cap_Bus;
+	this.ayudante = ayudante;
+	this.chofer = chofer;
+	this.estado = estado;
+}
+public void crear_Socio(Socio socio) {
+	Socio soc= new Socio();
+	soc= socio;
+}
+public int getCod_Socio() {
+	return cod_Socio;
+}
+public void setCod_Socio(int cod_Socio) {
+	this.cod_Socio = cod_Socio;
+}
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public String getApellidos() {
+	return apellidos;
+}
+public void setApellidos(String apellidos) {
+	this.apellidos = apellidos;
+}
+public String getLinea() {
+	return linea;
+}
+public void setLinea(String linea) {
+	this.linea = linea;
+}
+public int getNum_Bus() {
+	return num_Bus;
+}
+public void setNum_Bus(int num_Bus) {
+	this.num_Bus = num_Bus;
+}
+public int getCap_Bus() {
+	return cap_Bus;
+}
+public void setCap_Bus(int cap_Bus) {
+	this.cap_Bus = cap_Bus;
+}
+public String getAyudante() {
+	return ayudante;
+}
+public void setAyudante(String ayudante) {
+	this.ayudante = ayudante;
+}
+public String getChofer() {
+	return chofer;
+}
+public void setChofer(String chofer) {
+	this.chofer = chofer;
+}
+public Boolean getEstado() {
+	return estado;
+}
+public void setEstado(Boolean estado) {
+	this.estado = estado;
+}
+
+}
